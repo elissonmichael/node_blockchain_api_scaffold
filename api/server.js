3811f86f3fbd8d0d7d78ceaa9b7b6f075f07350d
Élisson Michael <elisson@les.inf.puc-rio.br>
2020-05-12 10:22:55 -0300

    Add Minimum Necessary Code for a Command

--- api/server.js
+++ api/server.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const fabric  = require('./fabric.js');
-
+const express    = require('express');
+const fabric     = require('./fabric.js');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/users/:id', async (req, res) => {
     let network  = await fabric.connectToNetwork('admin');
     let response = await network.contract.evaluateTransaction('readUser', req.params.id);
-    let json     = await JSON.parse(response);
                    await network.gateway.disconnect();
-    res.send(json);
+    res.send(response);
+});
+
+app.post('/users', async (req, res) => {
+    let network  = await fabric.connectToNetwork('admin');
+    let response = await network.contract.submitTransaction('createUser', req.body.id, req.body.value);
+                   await network.gateway.disconnect();
+    res.send(response);
 });
 
 app.listen(8081, () => console.log(`Server Started on PORT 8081`));
